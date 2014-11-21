371cd534c6f742ae3626277486999ca7c04a3fc0
samuelj <samuelj@avi.com>
2014-11-21 16:21:34 -0500

    Updates doc-md to support changes in format and implements table of contents.
    Now reads the title for each section from the properties.yml file, rather than from the headers within the markdown file. This allows the document to keep the headers consistent throughout, rather than relying on the user to ensure consistency.
    
    Implements a table of contents.
    
    Adds a hook for user-defined css.
    
    Replaces the grunt-bower-concat task with grunt-bower-task and grunt-contrib-concat to accomplish the same thing, only with more control.
    
    Adds a bower.json file for configuring bower dependencies.

--- tasks/doc-md.js
+++ tasks/doc-md.js
@@ -15,54 +15,99 @@ module.exports = function(grunt) {
 
     var idRegex = /[^\w]/g;
 
-    var grabMarkdown = function(/*String*/ basePath, /*String*/ fileName) {
+    var parseDocumentStructure = function(/*String*/ basePath, /*String*/ fileName, /*numeric*/ depth) {
         var fullPath = path.join(basePath, fileName);
 
         if (fs.statSync(fullPath).isDirectory()) {
             var subContent = {};
             fs.readdirSync(fullPath).forEach(function(subFile) {
-                subContent[subFile] = grabMarkdown(fullPath, subFile);
+                subContent[subFile] = parseDocumentStructure(fullPath, subFile, depth + 1);
             });
             return subContent;
         } else {
             if (fileName === "properties.yml") {
-                return yaml.safeLoad(fs.readFileSync(fullPath));
+                var properties = yaml.safeLoad(fs.readFileSync(fullPath));
+                properties['depth'] = depth;
+                return properties;
             } else {
-                return '' + fs.readFileSync(fullPath, {"encoding": "utf-8"});
+                return '' + fullPath;
             }
         }
     };
 
     var buildAndLinkHtml = function(content) {
         var order = content["properties.yml"]["toc"];
-        var html = '';
+        var depth = content["properties.yml"]["depth"] + 1;
+        if (depth > 4) {
+            depth = 4;
+        }
 
-        //TODO figure out how we're constructing table of contents. A template should probably be used.
-        var toc = '';
+        var name = content["properties.yml"]["name"];
+        var anchorId = name.replace(idRegex, '');
+        var html = buildHeader(name, anchorId, depth);
+
+        var tocSections = cheerio.load('');
 
         order.forEach(function(section) {
             var sectionHtml;
-            if (typeof content[section] === "string") {
-                sectionHtml = cheerio.load(converter.makeHtml(content[section]));
-                var sectionHeader = sectionHtml('h1, h2, h3, h4, h5, h6');
-                var anchorId = sectionHeader.text().replace(idRegex, '');
-                sectionHeader.attr('id', anchorId);
-                html = html + sectionHtml.html();
+            var markdown;
+            var sectionFile = content[section.file];
+
+
+            if (typeof  sectionFile === "string") {
+                var name = section.name;
+                var anchorId = name.replace(idRegex, '');
+                markdown = fs.readFileSync(sectionFile, {"encoding": "utf-8"});
+                sectionHtml = buildHeader(name, anchorId, depth + 1) + converter.makeHtml(markdown);
+                appendTocElement(tocSections, buildSimpleTocLink(name, anchorId));
             } else {
-                if (content[section]["base.md"]) {
-                    html = html + converter.makeHtml(content[section]["base.md"]);
+
+                if (sectionFile["base.md"]) {
+                    markdown = fs.readFileSync(sectionFile["base.md"], {"encoding": "utf-8"});
+                    sectionHtml = converter.makeHtml(markdown);
                 }
 
                 //this feels clunky. there's probably a better way to handle this
-                var subContent = buildAndLinkHtml(content[section]);
-                html = html + subContent["main"];
-                toc = toc + subContent["toc"];
+                var subContent = buildAndLinkHtml(sectionFile);
+                sectionHtml = sectionHtml + subContent["main"];
+                appendTocElement(tocSections, subContent["toc"]);
             }
-        }, '');
+
+            html = html + sectionHtml;
+        });
+
+        var toc = cheerio.load('<li></li>');
+        appendTocElement(toc, buildSimpleTocLink(name, anchorId));
+        var tocContents = cheerio.load('<ul>');
+        tocContents('ul').addClass('docmd-toc-list')
+            .append(tocSections.html());
+        toc.root().append(tocContents.html());
+
+
         return {
             "main": html,
-            "toc": toc
+            "toc": toc.html()
         };
+    };
+
+    var buildHeader = function(name, id, depth) {
+        var header = '<h' + (depth) + ' id="' + id + '">' + name + '</h' + depth + '>';
+        return header;
+    };
+
+    var appendTocElement = function(toc, link) {
+        var element = cheerio.load('<li></li>');
+        element('li')
+            .addClass('docmd-toc-item')
+            .append(link);
+        toc.root().append(element.html());
+    };
+
+    var buildSimpleTocLink = function(name, anchorId) {
+        return cheerio.load('<a>')('a')
+            .attr('href', '#' + anchorId)
+            .addClass("docmd-toc-link")
+            .text(name);
     };
 
 
@@ -71,11 +116,11 @@ module.exports = function(grunt) {
         var files = fs.readdirSync(docDir);
         var content = {};
         files.forEach(function (file) {
-            content[file] = grabMarkdown(docDir, file);
+            content[file] = parseDocumentStructure(docDir, file, 0);
         });
         var properties = content["properties.yml"];
         var htmlContent = buildAndLinkHtml(content);
-        var compiledTemplateName = "index.html";
+        var compiledTemplateName = markdownDir + ".html";
         var render = jade.compileFile(path.join(options.webDir, "index.jade"));
         var output = render({
             "content": htmlContent,
@@ -87,9 +132,15 @@ module.exports = function(grunt) {
     grunt.registerMultiTask('doc_md', function() {
         var options = this.options({
             webDir: path.join(__dirname, "../webpage"),
+            bowerDir: path.join(__dirname, "../bower_lib"),
             resourcesName: "resources"
         });
         var dataDir = this.data.directory;
+        var base = grunt.option('base') || process.cwd();
+
+        grunt.registerTask('docmd_chdir', function() {
+            process.chdir(base);
+        });
 
         if (!options.docs) {
             grunt.warn("docs directory not specified");
@@ -103,7 +154,9 @@ module.exports = function(grunt) {
 
         grunt.loadNpmTasks('grunt-contrib-clean');
         grunt.loadNpmTasks('grunt-bower-concat');
+        grunt.loadNpmTasks('grunt-contrib-concat');
         grunt.loadNpmTasks('grunt-contrib-copy');
+        grunt.loadNpmTasks('grunt-bower-task');
 
         //setup directories and libraries
         grunt.config('clean', {
@@ -120,13 +173,43 @@ module.exports = function(grunt) {
         grunt.task.run('clean:docmd_lib', 'clean:docmd_output');
         grunt.file.mkdir(options.output);
         grunt.file.mkdir(path.join(options.webDir, 'lib'));
-        grunt.config('bower_concat', {
-            docmd: {
-                dest: path.join(options.webDir, 'lib', 'lib.js'),
-                cssDest: path.join(options.webDir, 'lib', 'lib.css')
+        process.chdir(path.join(__dirname, '../'));
+        grunt.config('bower', {
+            'options': {
+                targetDir: 'bower_lib'
+            },
+            'install': {}
+        });
+        grunt.task.run('bower');
+        grunt.task.run('docmd_chdir');
+
+        grunt.config('concat', {
+
+            'docmd_js': {
+                options: {
+                    separator: ';\n'
+                },
+                src: [
+                    path.join(options.bowerDir, 'js', 'jquery', 'jquery.min.js'),
+                    path.join(options.bowerDir, 'js', 'bootstrap', 'bootstrap.min.js')
+                ],
+                dest: path.join(options.webDir, 'lib', 'lib.js')
+            },
+            'docmd_css': {
+                src: [
+                    path.join(options.bowerDir, 'css', '*', '*.css')
+                ],
+                dest: path.join(options.webDir, 'lib', 'lib.css')
+            },
+            'docmd_user': {
+                src: [
+                    path.join(options.cssDir, '*.css')
+                ],
+                dest: path.join(options.webDir, 'lib', 'user.css')
             }
         });
-        grunt.task.run('bower_concat:docmd');
+        grunt.task.run('concat:docmd_js', 'concat:docmd_css', 'concat:docmd_user');
+
 
         //TODO convert the markdown and apply templates
         grunt.registerTask('docmd_markdown', function() {
@@ -135,7 +218,7 @@ module.exports = function(grunt) {
         grunt.task.run('docmd_markdown');
 
         grunt.config('copy', {
-            docmd: {
+            docmd_resources: {
                 files: [
                     {
                         expand: true,
@@ -147,11 +230,15 @@ module.exports = function(grunt) {
             }
 
         });
-        grunt.task.run('copy:docmd');
+        grunt.task.run('copy:docmd_resources');
     });
 
 
-
+    /**
+     * Prints out the keys of an object. This is only for testing, and can
+     * probably be removed
+     * @param object the object whose keys should be printed
+     */
     var check = function(object) {
         if (! object) {
             grunt.log.writeln('object is falsey');
