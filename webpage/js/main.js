8a03a048bc62cdc6aec4aab4de43d0447586edeb
samuelj <samuelj@avi.com>
2014-12-01 13:00:44 -0500

    Hides toc initially when loaded on a small screen

--- webpage/js/main.js
+++ webpage/js/main.js
@@ -4,9 +4,13 @@
 
     toc.stick_in_parent()
         .on('sticky_kit:stick', function(e) {
+            console.log("toggle display: " + toggle.css('display'));
             toc.parent()
                 .attr('id', 'sticky-toc-parent')
                 .addClass('collapse in');
+            if (toggle.css('display') !== 'none') {
+                toggle.trigger('click');
+            }
         });
 
 
