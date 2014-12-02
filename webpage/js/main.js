038380ccb3e07d8626850cb2135c905cb937b981
samuelj <samuelj@avi.com>
2014-12-02 10:51:06 -0500

    Fixes layout issues Because sticky-kit modifies the dom, particularly relating to the parent of the item to be stuck, it wasn't playing well with the bootstrap layout. By specifying a parent, rather than letting sticky-kit choose its own, we can make sure the bootstrap column classes remain in the correct place.
    This also fixes the problem of the table of contents remaining hidden (with no way to show it again) when the window was resized from narrow to wide with the table of contents hidden.

--- webpage/js/main.js
+++ webpage/js/main.js
@@ -2,17 +2,29 @@
     var toc = $('#toc');
     var toggle = $('#guide-toc-toggle');
 
-    toc.stick_in_parent()
-        .on('sticky_kit:stick', function(e) {
-            console.log("toggle display: " + toggle.css('display'));
-            toc.parent()
-                .attr('id', 'sticky-toc-parent')
-                .addClass('collapse in');
-            if (toggle.css('display') !== 'none') {
-                toggle.trigger('click');
+    var first = true;
+
+    toc.stick_in_parent({
+        "parent": "#docmd-content"
+    }).on('sticky_kit:stick', function(e) {
+            if (first) {
+                first = false;
+                console.log("toggle display: " + toggle.css('display'));
+                toc.parent()
+                    .attr('id', 'sticky-toc-parent')
+                    .addClass('collapse in');
+                if (toggle.css('display') !== 'none') {
+                    toggle.trigger('click');
+                }
             }
+
         });
 
+    $(window).resize(function() {
+        if (! ($('#guide-toc-toggle:visible').length)) {
+            toc.parent().addClass('in');
+        }
+    });
 
     toggle.attr('data-target', '#sticky-toc-parent');
 
